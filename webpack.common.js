4b4ad4ea18089bfd5814d641fc631fbac0c8de3a
mhdwahyusiregar <siregarwahyu573@gmail.com>
2023-06-06 08:51:11 +0700

    perbaikan pada service worker & pada tombol skip to content

--- webpack.common.js
+++ webpack.common.js
@@ -47,6 +47,21 @@ module.exports = {
     }),
     new WorkboxWebpackPlugin.GenerateSW({
       swDest: './sw.bundle.js',
+      skipWaiting: true,
+      clientsClaim: true,
+      runtimeCaching: [
+        {
+          // eslint-disable-next-line prefer-regex-literals
+          urlPattern: new RegExp('^https://restaurant-api.dicoding.dev/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'RestaurantMWS-V1',
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        },
+      ],
     }),
   ],
 };
